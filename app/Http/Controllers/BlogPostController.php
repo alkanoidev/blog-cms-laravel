a4b6f047a209b1900d5926a324bc57d463aca74d
Filip Ivanovic <alkanoidev@gmail.com>
2022-12-25 22:04:43 +0100

    radi cuvanje ali greska

--- app/Http/Controllers/BlogPostController.php
+++ app/Http/Controllers/BlogPostController.php
@@ -4,16 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\BlogPost;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class BlogPostController extends Controller
 {
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         $blog = new BlogPost;
-        error_log($request);
-        $blog->content_html = $request->content;
+        Log::error($request->content."----------------------");
         $blog->title = "aaa";
+        $blog->content = $request->content;
         $blog->user_id = "123123";
         $blog->save();
+
         return redirect('/dashboard');
     }
 }
--- database/migrations/2022_12_23_165603_create_blogpost_table.php
+++ database/migrations/2022_12_23_165603_create_blogpost_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('blogpost', function (Blueprint $table) {
             $table->id();
             $table->string("title");
-            $table->string("content_html");
+            $table->string("content");
             $table->string("user_id");
             $table->timestamps();
         });
