a27bc66cfe3121881efba923f36be589c594b1d2
Filip Ivanovic <alkanoidev@gmail.com>
2022-12-30 10:49:59 +0100

    assets fix on nested routes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,21 +4,21 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="apple-touch-icon" sizes="76x76" href="/img/apple-icon.png">
-    <link rel="icon" type="image/png" href="/img/favicon.png">
+    <link rel="apple-touch-icon" sizes="76x76" href={{asset("/img/apple-icon.png")}}>
+    <link rel="icon" type="image/png" href={{asset("/img/favicon.png")}}>
     <title>
         Argon Dashboard 2 by Creative Tim
     </title>
     <!--     Fonts and icons     -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
     <!-- Nucleo Icons -->
-    <link href="./assets/css/nucleo-icons.css" rel="stylesheet" />
-    <link href="./assets/css/nucleo-svg.css" rel="stylesheet" />
+    <link href={{asset("/assets/css/nucleo-icons.css")}} rel="stylesheet" />
+    <link href={{asset("/assets/css/nucleo-svg.css")}} rel="stylesheet" />
     <!-- Font Awesome Icons -->
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-    <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
+    <link href={{asset("assets/css/nucleo-svg.css")}} rel="stylesheet" />
     <!-- CSS Files -->
-    <link id="pagestyle" href="assets/css/argon-dashboard.css" rel="stylesheet" />
+    <link id="pagestyle" href={{asset("assets/css/argon-dashboard.css")}} rel="stylesheet" />
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
@@ -61,10 +61,10 @@
     @endauth
 
     <!--   Core JS Files   -->
-    <script src="assets/js/core/popper.min.js"></script>
-    <script src="assets/js/core/bootstrap.min.js"></script>
-    <script src="assets/js/plugins/perfect-scrollbar.min.js"></script>
-    <script src="assets/js/plugins/smooth-scrollbar.min.js"></script>
+    <script src={{asset("assets/js/core/popper.min.js")}}></script>
+    <script src={{asset("assets/js/core/bootstrap.min.js")}}></script>
+    <script src={{asset("assets/js/plugins/perfect-scrollbar.min.js") }} ></script>
+    <script src={{asset("assets/js/plugins/smooth-scrollbar.min.js")}}></script>
     <script>
         var win = navigator.platform.indexOf('Win') > -1;
         if (win && document.querySelector('#sidenav-scrollbar')) {
@@ -95,7 +95,7 @@
     <script src="https://code.jquery.com/jquery-3.6.3.min.js"
         integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@editorjs/editorjs@latest"></script>
-    <script type="module" src="assets/js/argon-dashboard.js"></script>
+    <script type="module" src={{asset("assets/js/argon-dashboard.js")}}></script>
 
     <script type="text/javascript">
         $(document).ready(function() {
