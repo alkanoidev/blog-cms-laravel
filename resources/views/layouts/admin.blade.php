3522a63385b3b8c07e639868be66c3ffcb3e93bf
alkanoidev <alkanoidev@gmail.com>
2023-03-29 10:38:41 +0200

    tailwind init

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -24,7 +24,6 @@
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,200" />
 
-    @vite('resources/css/app.css')
     @yield('head')
 </head>
 
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>hello</h1>
+<h1 class="text-red-700 text-2xl font-bold">hello</h1>
 @endsection
