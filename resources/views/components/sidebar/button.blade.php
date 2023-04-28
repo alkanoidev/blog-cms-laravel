c9c756ddcdd0ccb2c4440d391cb636c98dc1c585
Filip Ivanovic <alkanoidev@gmail.com>
2023-04-28 19:01:10 +0200

    styles states

--- resources/views/components/sidebar/button.blade.php
+++ resources/views/components/sidebar/button.blade.php
@@ -1,9 +1,16 @@
 @props(['selected' => null, 'icon' => null, 'title' => null])
-<a href="#" class="{{ $selected ? 'bg-primary-container-light dark:bg-primary-container-dark' : 'bg-none' }} flex px-5 py-4 w-full rounded-full">
+<a href="#"
+    class="{{ $selected
+        ? 'dark:bg-primary-container-dark dark:text-on-primary-container-dark bg-primary-container-light text-on-primary-container-light 
+            dark:hover:bg-on-primary-dark hover:bg-[#b5cfeb]
+            dark:focus:bg-on-primary-dark focus:bg-[#a4bfdb] focus:outline-none'
+        : 'bg-none' }} 
+        flex px-5 py-4 w-full rounded-full transition">
     <div class="space-x-2 flex items-center">
         <span class="material-symbols-rounded h-6 w-6 text-primary-container-dark dark:text-primary-container-light">
             {{ $icon }}
         </span>
-        <span class="text-lg font-semibold text-primary-container-dark dark:text-primary-container-light">{{ $title }}</span>
+        <span
+            class="text-lg font-semibold text-primary-container-dark dark:text-primary-container-light">{{ $title }}</span>
     </div>
 </a>
