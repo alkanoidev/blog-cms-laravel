664c0729b26cfb5f37ded63753f30904e09e4acd
Filip Ivanovic <alkanoidev@gmail.com>
2023-07-11 16:46:51 +0200

    styles: 404 light theme

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Najjaci blog</title>
+    <title>{{ config('app.name') }} | 404 Došlo je do greške</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,200" />
@@ -22,10 +22,10 @@
 <body class="{{ $class ?? '' }} bg-light dark:bg-dark h-full w-full grid place-content-center">
     <main class="text-center space-y-4">
         <div>
-            <h1 class="text-3xl">Došlo je do greške</h1>
+            <h1 class="text-3xl text-on-light dark:text-on-dark">Došlo je do greške</h1>
             <h2 class="text-3xl text-primary-light dark:text-primary-dark font-bold">404</h2>
         </div>
-        <p class="max-w-sm">Stranica nije pronađena. Moguće je da tražena stranica više ne postoji ili je link
+        <p class="max-w-sm text-on-light dark:text-on-dark">Stranica nije pronađena. Moguće je da tražena stranica više ne postoji ili je link
             pogrešan/promenjen.</p>
         <x-buttons.button-filled title="Nazad" href="{{ url()->previous() }}" />
     </main>
