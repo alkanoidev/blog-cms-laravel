c2b30944c51eb67c6e6eb28ff531b8d54f9b15aa
Filip Ivanovic <alkanoidev@gmail.com>
2023-07-11 16:28:05 +0200

    fix: app title on pages

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,7 +7,7 @@
     <link rel="apple-touch-icon" sizes="76x76" href={{ asset('/img/apple-icon.png') }}>
     <link rel="icon" type="image/png" href={{ asset('/img/favicon.png') }}>
     <title>
-        Najjaci Blog | Dashboard
+        {{ config('app.name') }} | Dashboard
     </title>
     <!--     Fonts and icons     -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Najjaci blog</title>
+    <title>{{ config('app.name') }} | Početna</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,200" />
