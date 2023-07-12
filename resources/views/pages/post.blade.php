a1be57c34997afa99e366dcd49d6d74a1f6e9d3f
Filip Ivanovic <alkanoidev@gmail.com>
2023-07-12 19:04:26 +0200

    styles: poppins font

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -17,7 +17,6 @@
             <img src="/img/ui/background.svg" class="w-full absolute -z-10 rounded-b-2xl hidden dark:block" alt="">
             <img src="/img/ui/background-light.svg" class="w-full absolute -z-10 rounded-b-2xl block dark:hidden"
                 alt="">
-
             <div
                 class="mx-auto 2xl:max-w-4xl xl:max-w-4xl lg:max-w-3xl md:max-w-md sm:max-w-sm max-w-5xl pt-4
                         flex flex-col gap-4
