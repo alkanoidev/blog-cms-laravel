0852c9b89bce90cead3208455228bbbedf7b77c6
Filip Ivanovic <alkanoidev@gmail.com>
2023-11-02 20:23:34 +0100

    styles dark mode

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,25 +1,18 @@
-@extends('layouts.app', ['title' => 'Login'])
+@extends('layouts.admin', ['class' => 'g-sidenav-show bg-gray-900'])
 
-@section('head')
+{{-- @section('head')
     <link id="pagestyle" href={{ asset('assets/css/argon-dashboard.css') }} rel="stylesheet" />
-@endsection
+@endsection --}}
 
 @section('content')
-    {{-- <div class="container position-sticky z-index-sticky top-0">
-        <div class="row">
-            <div class="col-12">
-                @include('layouts.navbars.guest.navbar')
-            </div>
-        </div>
-    </div>  --}}
     <main class="main-content mt-0">
         <section>
             <div class="page-header vh-100">
                 <div class="container">
                     <div class="d-flex h-100 justify-content-center align-items-center">
-                        <div class="card">
-                            <div class="card-header pb-0 text-start">
-                                <h4 class="font-weight-bolder">Sign In</h4>
+                        <div class="card text-white bg-gray-800">
+                            <div class="card-header pb-0 text-start bg-gray-800">
+                                <h4 class="font-weight-bolder text-white">Sign In</h4>
                                 <p class="mb-0">Enter your email and password to sign in</p>
                             </div>
                             <div class="card-body">
@@ -27,7 +20,7 @@
                                     @csrf
                                     @method('post')
                                     <div class="flex flex-col mb-3">
-                                        <label for="email">Email</label>
+                                        <label for="email" class="text-light">Email</label>
                                         <input type="email" name="email" class="form-control form-control-lg"
                                             aria-label="Email">
                                         @error('email')
@@ -35,7 +28,7 @@
                                         @enderror
                                     </div>
                                     <div class="flex flex-col mb-3">
-                                        <label for="password">Password</label>
+                                        <label for="password" class="text-light">Password</label>
                                         <input type="password" name="password" class="form-control form-control-lg"
                                             aria-label="Password">
                                         @error('password')
@@ -44,7 +37,7 @@
                                     </div>
                                     <div class="form-check form-switch">
                                         <input class="form-check-input" name="remember" type="checkbox" id="rememberMe">
-                                        <label class="form-check-label" for="rememberMe">Remember me</label>
+                                        <label class="form-check-label text-light" for="rememberMe">Remember me</label>
                                     </div>
                                     <div class="text-center">
                                         <button type="submit" class="btn btn-lg btn-primary w-100 mt-4 mb-0">Sign
@@ -55,14 +48,13 @@
                             <div class="card-footer text-center pt-0 px-lg-2 px-1">
                                 <p class="mb-1 text-sm mx-auto">
                                     Forgot you password? Reset your password
-                                    <a href="{{ route('reset-password') }}"
-                                        class="text-gradient-dark font-weight-bold">here</a>
+                                    <a href="{{ route('reset-password') }}" class="text-primary font-weight-bold">here</a>
                                 </p>
                             </div>
                             <div class="card-footer text-center pt-0 px-lg-2 px-1">
                                 <p class="mb-4 text-sm mx-auto">
                                     Don't have an account?
-                                    <a href="{{ route('register') }}" class="text-gradient-dark font-weight-bold">Sign
+                                    <a href="{{ route('register') }}" class="font-weight-bold text-primary">Sign
                                         up</a>
                                 </p>
                             </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,8 +1,8 @@
-@extends('layouts.admin', ['title' => 'Register'])
+@extends('layouts.admin', ['class' => 'g-sidenav-show bg-gray-900'])
 
-@section('head')
+{{-- @section('head')
     <link id="pagestyle" href={{ asset('assets/css/argon-dashboard.css') }} rel="stylesheet" />
-@endsection
+@endsection --}}
 
 @section('content')
     {{-- @include('layouts.navbars.guest.navbar') --}}
@@ -14,8 +14,7 @@
                 <div class="row justify-content-center">
                     <div class="col-lg-5 text-center mx-auto">
                         <h1 class="text-white mb-2 mt-5">Welcome!</h1>
-                        <p class="text-lead text-white">Use these awesome forms to login or create new account in your
-                            project for free.</p>
+                        <p class="text-lead text-white">Register as a writer and wait for the administration to approve!</p>
                     </div>
                 </div>
             </div>
@@ -23,45 +22,48 @@
         <div class="container">
             <div class="row mt-lg-n10 mt-md-n11 mt-n10 justify-content-center">
                 <div class="col-xl-4 col-lg-5 col-md-7 mx-auto">
-                    <div class="card z-index-0">
-                        <div class="card-header text-center pt-4 pb-0">
-                            <h5>Register</h5>
+                    <div class="card z-index-0 text-white bg-gray-800">
+                        <div class="card-header text-center pt-4 pb-0 text-white bg-gray-800">
+                            <h4 class="text-white">Register</h4>
                         </div>
                         <div class="card-body">
                             <form method="POST" action="{{ route('register.perform') }}">
                                 @csrf
                                 <div class="flex flex-col mb-3">
-                                    <input type="text" name="firstname" class="form-control" placeholder="First Name"
-                                        aria-label="Name" value="{{ old('firstname') }}">
+                                    <label for="email" class="text-light">First Name</label>
+                                    <input type="text" name="firstname" class="form-control" aria-label="Name"
+                                        value="{{ old('firstname') }}">
                                     @error('firstname')
                                         <p class='text-danger text-xs pt-1'> {{ $message }} </p>
                                     @enderror
                                 </div>
                                 <div class="flex flex-col mb-3">
-                                    <input type="text" name="lastname" class="form-control" placeholder="Last Name"
-                                        aria-label="Name" value="{{ old('lastname') }}">
+                                    <label for="email" class="text-light">Last Name</label>
+                                    <input type="text" name="lastname" class="form-control" aria-label="Name"
+                                        value="{{ old('lastname') }}">
                                     @error('lastname')
                                         <p class='text-danger text-xs pt-1'> {{ $message }} </p>
                                     @enderror
                                 </div>
                                 <div class="flex flex-col mb-3">
-                                    <input type="email" name="email" class="form-control" placeholder="Email"
-                                        aria-label="Email" value="{{ old('email') }}">
+                                    <label for="email" class="text-light">Email</label>
+                                    <input type="email" name="email" class="form-control" aria-label="Email"
+                                        value="{{ old('email') }}">
                                     @error('email')
                                         <p class='text-danger text-xs pt-1'> {{ $message }} </p>
                                     @enderror
                                 </div>
                                 <div class="flex flex-col mb-3">
-                                    <input type="password" name="password" class="form-control" placeholder="Password"
-                                        aria-label="Password">
+                                    <label for="email" class="text-light">Password</label>
+                                    <input type="password" name="password" class="form-control" aria-label="Password">
                                     @error('password')
                                         <p class='text-danger text-xs pt-1'> {{ $message }} </p>
                                     @enderror
                                 </div>
                                 <div class="form-check form-check-info text-start">
                                     <input class="form-check-input" type="checkbox" name="terms" id="flexCheckDefault">
-                                    <label class="form-check-label" for="flexCheckDefault">
-                                        I agree the <a href="javascript:;" class="text-dark font-weight-bold">Terms and
+                                    <label class="form-check-label text-light" for="flexCheckDefault">
+                                        I agree the <a href="javascript:;" class="text-primary font-weight-bold">Terms and
                                             Conditions</a>
                                     </label>
                                     @error('terms')
@@ -72,7 +74,7 @@
                                     <button type="submit" class="btn btn-lg btn-primary w-100 my-4 mb-2">Sign up</button>
                                 </div>
                                 <p class="text-sm mt-3 mb-0">Already have an account? <a href="{{ route('login') }}"
-                                        class="text-gradient-dark font-weight-bold">Sign in</a></p>
+                                        class="text-primary font-weight-bold">Sign in</a></p>
                             </form>
                         </div>
                     </div>
@@ -82,3 +84,78 @@
     </main>
     @include('layouts.footers.guest.footer')
 @endsection
+
+<style>
+    /* Hide the default checkbox */
+    .container1 input {
+        position: absolute;
+        opacity: 0;
+        cursor: pointer;
+        height: 0;
+        width: 0;
+    }
+
+    .container1 {
+        display: flex;
+        gap: 10px;
+    }
+
+
+    /* Create a custom checkbox */
+    .checkmark {
+        position: relative;
+        box-shadow: rgb(255, 84, 0) 0px 0px 0px 2px;
+        background-color: rgba(16, 16, 16, 0.5);
+        height: 20px;
+        width: 20px;
+        margin-right: 10px;
+        flex-shrink: 0;
+        margin-top: -1px;
+        transition: all 0.2s ease 0s;
+        cursor: pointer;
+        transform-origin: 0px 10px;
+        border-radius: 4px;
+        margin: -1px 10px 0px 0px;
+        padding: 0px;
+        box-sizing: border-box;
+    }
+
+    .container1 input:checked~.checkmark {
+        box-shadow: rgb(255, 84, 0) 0px 0px 0px 2px;
+        background-color: rgba(245, 24, 24, 0.5);
+        height: 20px;
+        width: 20px;
+        margin-right: 10px;
+        flex-shrink: 0;
+        margin-top: -1px;
+        transition: all 0.2s ease 0s;
+        cursor: pointer;
+        transform-origin: 0px 10px;
+        border-radius: 4px;
+        margin: -1px 10px 0px 0px;
+        padding: 0px;
+        box-sizing: border-box;
+    }
+
+    .checkmark:after {
+        content: "";
+        position: absolute;
+        display: none;
+    }
+
+    .container1 input:checked~.checkmark:after {
+        display: block;
+    }
+
+    /* Style the checkmark/indicator */
+    .container1 .checkmark:after {
+        left: 0.45em;
+        top: 0.25em;
+        width: 0.25em;
+        height: 0.5em;
+        border: solid white;
+        border-width: 0 0.15em 0.15em 0;
+        transform: rotate(45deg);
+        transition: all 500ms ease-in-out;
+    }
+</style>
