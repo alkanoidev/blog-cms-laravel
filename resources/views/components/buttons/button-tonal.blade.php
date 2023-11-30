262b6a250208f101eedb8650042a6260d3ea10bc
Filip Ivanovic <alkanoidev@gmail.com>
2023-11-30 18:34:05 +0100

    styles: tonal button

--- resources/views/components/buttons/button-tonal.blade.php
+++ resources/views/components/buttons/button-tonal.blade.php
@@ -2,7 +2,7 @@
 <a aria-label={{ $title }} href={{ $href }}
     class="py-3 px-6 text-center {{ $classes }} rounded-full font-semibold whitespace-nowrap
           inline-flex gap-2 justify-center
-        dark:bg-primary-container-dark/20 bg-primary-container-light/20
+        dark:bg-primary-container-dark/40 bg-primary-container-light/40
         dark:text-on-primary-container-dark  text-on-primary-container-light
         dark:hover:bg-on-primary-dark hover:bg-[#b5cfeb]
         dark:focus:bg-on-primary-dark focus:bg-[#a4bfdb] focus:outline-none">
