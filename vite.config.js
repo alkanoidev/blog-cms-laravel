e7cd365d69f4f9c65e191e0da3e9e89edd954883
Filip Ivanovic <alkanoidev@gmail.com>
2023-12-12 20:07:12 +0100

    build: css input

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/scss/argon-dashboard.css', 'resources/js/app.js'],
+            input: ['public/assets/css/argon-dashboard.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
