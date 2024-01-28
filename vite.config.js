38a308583c34d9444db5febece041f2319479fa9
Filip Ivanovic <alkanoidev@gmail.com>
2024-01-28 19:24:45 +0100

    feat: add argon css to vite conf

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['public/assets/css/argon-dashboard.css', 'resources/js/app.js'],
+            input: ['public/assets/css/argon-dashboard.css', 'resources/js/app.js', 'resources/css/app.css', 'resources/scss/argon-dashboard.scss'],
             refresh: true,
         }),
     ],
